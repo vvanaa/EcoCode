f2605c49030a1fedf9edd45adef036a865ea1024
Pelin Top <ptop@chapman.edu>
2023-10-09 00:19:40 -0700

    update on custom_blocks.js - new version

---
+++ design-blocks/custom_blocks.js
@@ -0,0 +1,60 @@
+// custom_blocks.js
+
+// Turn Off Lights
+Blockly.Blocks['turn_off_lights'] = {
+  init: function() {
+    this.appendDummyInput().appendField("Turn Off Lights");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("Turn off lights when not in use.");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['turn_off_lights'] = function(block) {
+  var code = 'console.log("Remember to turn off lights when not in use.");\n';
+  code += 'let energy_tips = getEnergyTips();  // Assuming a getEnergyTips function in JS\n';
+  code += 'if (energy_tips) {\n    console.log(energy_tips);\n}\n';
+  return code;
+};
+
+// Motion Sensor
+Blockly.Blocks['motion_sensor'] = {
+  init: function() {
+    this.appendDummyInput().appendField("Motion Sensor");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(210);
+    this.setTooltip("Learn about motion sensors for energy efficiency.");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['motion_sensor'] = function(block) {
+  var code = 'console.log("Benefits of motion sensors for energy efficiency:");\n';
+  code += 'console.log("Motion sensors reduce energy waste by turning off lights when not needed.");\n';
+  return code;
+};
+
+// Reduce Water Usage
+Blockly.Blocks['reduce_water_usage'] = {
+  init: function() {
+    this.appendDummyInput().appendField("Reduce Water Usage");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(160);
+    this.setTooltip("Reduce water consumption with practical tips.");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['reduce_water_usage'] = function(block) {
+  var code = 'console.log("Practical suggestions to reduce water consumption:");\n';
+  code += 'let water_tips = getWaterSavingTips();  // Assuming a getWaterSavingTips function in JS\n';
+  code += 'if (water_tips) {\n    console.log(water_tips);\n}\n';
+  return code;
+};
+
+
+
